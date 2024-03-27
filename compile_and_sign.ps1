090b944b6f3ea238fdcd02fb4785939e9284f9a5
Olson <olsonb97@gmail.com>
2024-03-27 09:36:16 -0500

    removed compilation script

--- compile_and_sign.ps1
+++
@@ -1,5 +0,0 @@
-$pyinstallerCommand = "pyinstaller --onefile --windowed -i '.\resources\quickparse.ico' --add-data '.\resources\quickparse.ico;.' --clean '.\qp_gui.py'"
-Start-Process -FilePath "powershell.exe" -ArgumentList "-Command", $pyinstallerCommand -NoNewWindow -Wait
-
-$cert = New-SelfSignedCertificate -DnsName "Brendan Olson" -CertStoreLocation "Cert:\CurrentUser\My" -Type CodeSigning -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3") -Subject "CN=Brendan Olson"
-Set-AuthenticodeSignature -FilePath ".\dist\qp_gui.exe" -Certificate $cert -TimestampServer "http://timestamp.digicert.com"
